61d2936e21f817c4d423633d975e08b0c072a007
nazid <nazidk773@gmail.com>
2024-08-21 12:14:01 +0530

    add or remove class

--- app.js
+++ app.js
@@ -4,3 +4,13 @@
 
 const btn = document.querySelector(".switch-btn");
 const video = document.querySelector("video-container");
+
+btn.addEventListener("click",function(){
+    if(!btn.classList.contains("slide")){
+        btn.classList.add("slide");
+        
+    }
+    else{
+        btn.classList.remove("slide");
+    }
+});
